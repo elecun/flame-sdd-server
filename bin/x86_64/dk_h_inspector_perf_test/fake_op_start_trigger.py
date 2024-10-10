93eb002ce3aae35ee1ae256543a56b200f1e4b0e
elecun <elecun2@gmail.com>
2024-10-10 19:40:04 +0900

    added for performance test

---
+++ bin/x86_64/dk_h_inspector_perf_test/fake_op_start_trigger.py
@@ -0,0 +1,16 @@
+
+import zmq
+import time
+import json
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://127.0.0.1:5008")
+
+time.sleep(1)
+topic = "manual_control"
+message = {
+    "op_tarigger":True
+}
+message_string = json.dumps(message)
+socket.send_multipart([topic.encode('utf-8'), message_string.encode('utf-8')])
