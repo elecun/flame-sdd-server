c12d19ce848e23aca9264faff2e137e8dab21147
elecun <elecun2@gmail.com>
2024-10-10 19:40:38 +0900

    moved from dk.ni.daq.handler

---
+++ components/ni.daq.pulse.generator/ni.daq.pulse.generator.cc
@@ -0,0 +1,164 @@
+
+#include "ni.daq.pulse.generator.hpp"
+#include <flame/log.hpp>
+#include <flame/config_def.hpp>
+
+using namespace flame;
+
+static ni_daq_pulse_generator* _instance = nullptr;
+flame::component::object* create(){ if(!_instance) _instance = new ni_daq_pulse_generator(); return _instance; }
+void release(){ if(_instance){ delete _instance; _instance = nullptr; }}
+
+bool ni_daq_pulse_generator::on_init(){
+
+    /* get info from profile */
+    _daq_device_name = get_profile()->parameters().value("daq_device_name", "Dev1");
+    _daq_counter_channel = get_profile()->parameters().value("daq_counter_channel", "ctr0");
+    _daq_pulse_freq = get_profile()->parameters().value("daq_pulse_freq", 30);
+    _daq_pulse_samples = get_profile()->parameters().value("daq_pulse_samples", 1000);
+    _daq_pulse_duty = get_profile()->parameters().value("daq_pulse_duty", 0.5);
+    logger::info("[{}] Camera Triggering via {}/{} at {}(samples:{}, duty:{})", get_name(), _daq_device_name, _daq_counter_channel, _daq_pulse_freq, _daq_pulse_samples, _daq_pulse_duty);
+
+
+
+    //_start_pulse_generation(_daq_pulse_freq, _daq_pulse_samples, _daq_pulse_duty);
+    thread subscriber = thread(&ni_daq_pulse_generator::_subscribe, this, get_profile()->parameters());
+    _subscriber_handle = subscriber.native_handle();
+    subscriber.detach();
+
+    return true;
+}
+
+void ni_daq_pulse_generator::on_loop(){
+
+    /* create message */
+    map<string, unsigned long long> daq_status;
+    daq_status.insert(make_pair("triggering", _triggering.load()));
+    json info = daq_status;
+    string status_message = info.dump();
+
+    /* camera grabbing info publish */
+    string topic = fmt::format("{}/{}", get_name(), "/status");
+    pipe_data topic_msg(topic.data(), topic.size());
+    pipe_data end_msg(status_message.data(), status_message.size());
+    get_port("status")->send(topic_msg, zmq::send_flags::sndmore);
+    get_port("status")->send(end_msg, zmq::send_flags::dontwait);
+    
+}
+
+void ni_daq_pulse_generator::on_close(){
+
+    _stop_pulse_generation();
+
+    /* cancel the subscriber thread */
+    _thread_stop_signal.store(true);
+    pthread_cancel(_subscriber_handle);
+    pthread_join(_subscriber_handle, nullptr);
+    
+}
+
+void ni_daq_pulse_generator::on_message(){
+    
+}
+
+
+bool ni_daq_pulse_generator::_start_pulse_generation(double freq, unsigned long long n_pulses, double duty){
+
+    string channel = fmt::format("{}/{}", _daq_device_name, _daq_counter_channel);
+
+    /* create task handle */
+    if(DAQmxCreateTask("camera_trigger", &_handle_pulsegen_task)){
+        logger::error("[{}] Failed to create DAQ task", get_name());
+        return false;
+    }
+
+    /* create counter config to generate pulse */
+    if(DAQmxCreateCOPulseChanFreq(_handle_pulsegen_task, channel.c_str(),  // device_name / counter_channel
+                                    "",               // Name of the virtual channel (optional)
+                                    DAQmx_Val_Hz,     // units
+                                    DAQmx_Val_Low,    // idle state
+                                    0.0,              // initial delay
+                                    freq,             // frequency
+                                    duty               // Duty cycle
+    )){
+        logger::error("[{}] Failed to create pulse channel", get_name());
+        DAQmxClearTask(_handle_pulsegen_task);
+        return false;
+    }
+
+    /* set timing for infinite pulse generation */
+    // Note! Finite sample : DAQmx_Val_FiniteSamps
+    // Note! Infinite sample : DAQmx_Val_ContSamps
+    if(DAQmxCfgImplicitTiming(_handle_pulsegen_task, DAQmx_Val_ContSamps, n_pulses)){
+        logger::error("[{}] Failed to configure timing", get_name());
+        DAQmxClearTask(_handle_pulsegen_task);
+        return false;
+    }
+
+    /* start to run task */
+    if(DAQmxStartTask(_handle_pulsegen_task)){
+        logger::error("[{}] Failed to run the pulse generation", get_name());
+        DAQmxClearTask(_handle_pulsegen_task);
+        return false;
+    }
+
+    logger::info("[{}] Started the camera trigger pulse generation", get_name());
+    _triggering.store(true);
+
+    return true;
+}
+
+void ni_daq_pulse_generator::_stop_pulse_generation(){
+
+    if(_handle_pulsegen_task){
+        if(DAQmxStopTask(_handle_pulsegen_task)) {
+            logger::error("[{}] Failed to stop the pulse generation task", get_name());
+        }
+        
+        DAQmxClearTask(_handle_pulsegen_task);
+        _handle_pulsegen_task = nullptr;
+
+        logger::info("[{}] Stopped the camera trigger pulse generation", get_name());
+        _triggering.store(false);
+    }
+    
+}
+
+void ni_daq_pulse_generator::_subscribe(json parameters){
+
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, nullptr);
+    pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, nullptr);
+
+    while(!_thread_stop_signal.load()){
+        try{
+            pipe_data topic;
+            pipe_data json_msg;
+
+            zmq::recv_result_t topic_result = get_port("manual_control")->recv(topic, zmq::recv_flags::none);
+            zmq::recv_result_t message_result = get_port("manual_control")->recv(json_msg, zmq::recv_flags::none);
+            if(message_result){
+                std::string message(static_cast<char*>(json_msg.data()), json_msg.size());
+                auto json_data = json::parse(message);
+
+                if(json_data.contains("op_trigger")){
+                    bool triggered = json_data["op_trigger"].get<bool>();
+                    if(triggered){
+                        _start_pulse_generation(_daq_pulse_freq, _daq_pulse_samples, _daq_pulse_duty);
+                        logger::info("[{}] Start generating camera triggering...", get_name());
+                    }
+                    else {
+                        _stop_pulse_generation();
+                        logger::info("[{}] Stop generating camera triggering...", get_name());
+                    }
+                }
+            }
+        }
+        catch(const json::parse_error& e){
+            logger::error("[{}] message cannot be parsed. {}", get_name(), e.what());
+        }
+        catch(const std::runtime_error& e){
+            logger::error("[{}] Runtime error occurred!", get_name());
+        }
+    }
+
+}
---
+++ components/ni.daq.pulse.generator/ni.daq.pulse.generator.hpp
@@ -0,0 +1,58 @@
+/**
+ * @file ni.daq.pulse.generator
+ * @author Byunghun Hwang <bh.hwang@iae.re.kr>
+ * @brief Trigger Signal Generator with NI DAQ 
+ * @version 0.1
+ * @date 2024-07-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef FLAME_NI_DAQ_PULSE_GENERATOR_HPP_INCLUDED
+#define FLAME_NI_DAQ_PULSE_GENERATOR_HPP_INCLUDED
+
+#include <flame/component/object.hpp>
+#include <string>
+#include <NIDAQmx.h>
+#include <atomic>
+
+class ni_daq_pulse_generator : public flame::component::object {
+    public:
+        ni_daq_pulse_generator() = default;
+        virtual ~ni_daq_pulse_generator() = default;
+
+        // default interface functions
+        bool on_init() override;
+        void on_loop() override;
+        void on_close() override;
+        void on_message() override;
+
+    private:
+        /* start or stop the pulse generation continuously */
+        bool _start_pulse_generation(double freq, unsigned long long n_pulses, double duty);
+        void _stop_pulse_generation();
+
+        void _subscribe(json parameters);
+
+    private:
+        string _daq_device_name {""};
+        string _daq_counter_channel {""};
+        double _daq_pulse_freq {30};
+        unsigned long long _daq_pulse_samples { 1000 };
+        double _daq_pulse_duty {0.5};
+        TaskHandle _handle_pulsegen_task { nullptr };
+
+        /* status */
+        std::atomic<bool> _triggering { false };
+
+        /* for manual control */
+        pthread_t _subscriber_handle;
+        std::atomic<bool> _thread_stop_signal { false };
+
+}; /* class */
+
+EXPORT_COMPONENT_API
+
+
+#endif
