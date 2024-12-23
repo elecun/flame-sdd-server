e5946209fc8e2aa8e90576a6c16e0f7de2204f45
elecun <elecun2@gmail.com>
2024-12-23 20:46:12 +0900

    fixed

--- monitor/surface_defect_monitor/window.py
+++ monitor/surface_defect_monitor/window.py
@@ -199,7 +199,7 @@ class AppWindow(QMainWindow):
                     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                     h, w, ch = rgb_image.shape
                     bytes_per_line = ch * w
-                    qt_image = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+                    qt_image = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
                     pixmap = QPixmap.fromImage(qt_image)
 
                     try:
