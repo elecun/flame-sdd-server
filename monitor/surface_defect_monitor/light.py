6fd33470d0baa883054b66f344d6d1715bf81ea1
elecun <elecun2@gmail.com>
2025-03-27 18:59:40 +0900

    updated

--- monitor/surface_defect_monitor/light.py
+++ monitor/surface_defect_monitor/light.py
@@ -2,18 +2,15 @@
 Light Control with DMX protocol
 """
 import socket
-from util.logger.console import ConsoleLogger
+import time
 
 class LightController:
     def __init__(self):
-        self.__console = ConsoleLogger.get_logger()
+        pass
 
-
-    def light_on(self, dest_ip:str, dest_port:int):
+    def light_on(id, self, dest_ip:str, dest_port:int):
         dmx_data = bytearray(512)
-        dmx_data[1] = 0x10  # 첫 채널 값을 255로 설정 (조명 켜짐)
-        dmx_data[7] = 0x10  # 0x3d 위치 채널 값을 255로 설정 (조명 켜짐)
-        dmx_data[9] = 0x10  # 추가로 켜진 조명 1
+        dmx_data[id] = 0x55  # 첫 채널 값을 255로 설정 (조명 켜짐)
 
         packet = self.__create_artnet_dmx_packet(sequence=0, physical=0, universe=0, data=dmx_data)
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,11 +18,9 @@ class LightController:
         sock.close()
         print("on")
 
-    def light_off(self, dest_ip:str, dest_port:int):
+    def light_off(id, self, dest_ip:str, dest_port:int):
         dmx_data = bytearray(512)
-        dmx_data[1] = 0x00  # 첫 채널 값을 255로 설정 (조명 켜짐)
-        dmx_data[7] = 0x00  # 0x3d 위치 채널 값을 255로 설정 (조명 켜짐)
-        dmx_data[9] = 0x00  # 추가로 켜진 조명 1
+        dmx_data[id] = 0x00  # 첫 채널 값을 255로 설정 (조명 켜짐)
 
         packet = self.__create_artnet_dmx_packet(sequence=0, physical=0, universe=0, data=dmx_data)
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -42,3 +37,14 @@ class LightController:
         length = len(data).to_bytes(2, byteorder='big')
         
         return header + opcode + protocol_version + sequence + physical + universe + length + data
+
+if __name__ == "__main__":
+    ids = [1,3,5,7,9,11,13,15,17,19]
+    controller = LightController()
+    for id in ids:
+        print(f"{id} on")
+        controller.light_on(id, "192.168.0.60", 6454)
+        time.sleep(1)
+        print(f"{id} off")
+        controller.light_off(id, "192.168.0.60", 6454)
+        time.sleep(1)
