ae2e34758d2f86334bca1a883d122009aec03c4b
elecun <elecun2@gamil.com>
2025-06-10 23:03:27 +0900

    sample for test

--- python/data_pub_test.py
+++ python/data_pub_test.py
@@ -9,24 +9,26 @@ socket.setsockopt(zmq.RCVBUF .RCVHWM, 1000)
 socket.setsockopt(zmq.LINGER, 0)
 socket.bind("tcp://*:5401")
 
-time.sleep(10)
+try:
+    while True:
 
-topic = "ni_daq_controller/line_signal"  # 구독 주제
-msg = {
-    "date":"20250401182801",
-    "mt_stand_height":350,
-    "mt_stand_width":350,
-    "sdd_in_path":"/home/dev/local_storage/20250401/20250401182801_350x350",
-    "sdd_out_path":"/home/dev/nas_storage/20250401/20250401182801_350x350"
-}
-j_string = json.dumps(msg)
-multipart_data = [topic.encode(), j_string.encode()]
-socket.send_multipart(multipart_data)  # 멀티파트 데이터 전송
-print("try 1")
+        topic = "test"  # 구독 주제
+        msg = {
+            "date":"20250401182801",
+            "mt_stand_height":350,
+            "mt_stand_width":350,
+            "sdd_in_path":"/home/dev/local_storage/20250401/20250401182801_350x350",
+            "sdd_out_path":"/home/dev/nas_storage/20250401/20250401182801_350x350"
+        }
+        j_string = json.dumps(msg)
+        multipart_data = [topic.encode(), j_string.encode()]
+        socket.send_multipart(multipart_data)  # 멀티파트 데이터 전송
+        print("send message")
 
-    
-
-
-# 소켓 및 컨텍스트 종료
-socket.close()
-context.term()
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("end publisher")
+finally:
+    # 소켓 및 컨텍스트 종료
+    socket.close()
+    context.term()
--- python/data_sub_test.py
+++ python/data_sub_test.py
@@ -5,20 +5,26 @@ import json
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.setsockopt_string(zmq.SUBSCRIBE, "focus_control")
-socket.connect("tcp://192.168.0.90:5104")
+socket.setsockopt(zmq.RCVBUF .RCVHWM, 1000)
+# socket.setsockopt(zmq.RCVTIMEO, 500)
+socket.setsockopt(zmq.LINGER, 0)
+socket.setsockopt(zmq.RECONNECT_IVL, 500)
+socket.connect("tcp://127.0.0.1:5401")
+socket.setsockopt_string(zmq.SUBSCRIBE, "test")
+
 
 try:
     while True:
-        time.sleep(3)
-        # 메시지 수신
-        topic, message = socket.recv_multipart()
-        print(topic, message)
-
-        
+        try:
+            # 메시지 수신
+            topic, message = socket.recv_multipart()
+            print(topic, message)
+        except zmq.error.ZMQError as e:
+            pass
 except KeyboardInterrupt:
     print("\n구독 종료")
+finally:
 
-# 소켓 및 컨텍스트 종료
-socket.close()
-context.term()
+    # 소켓 및 컨텍스트 종료
+    socket.close()
+    context.term()
